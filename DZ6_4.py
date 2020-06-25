df77ddb222ace2ca48c4392e80cb0938a00ba433
PythonOleg <66390707+PythonOleg@users.noreply.github.com>
2020-06-25 23:17:35 +0300

    Add files via upload

--- DZ6_4.py
+++ DZ6_4.py
@@ -1,22 +1,31 @@
-def decorator(func):
-    def wrapper(s):
-        text_b = func(s)
-        text_i = func(s)
-        text_u = func(s)
-        
+def dec1(func1):
+    def wrapper1(s):
+        text_b = func1(s)
         text_in_b = ('<b> ' + str(text_b) + ' </b>')
-        text_in_i = ('<i> ' + str(text_i) + ' </i>')
-        text_in_u = ('<u> ' + str(text_u) + ' </u>')
         print(text_in_b)
+    return wrapper1
+
+def dec2(func2):
+    def wrapper2(s):
+        text_i = func2(s)
+        text_in_i = ('<i> ' + str(text_i) + ' </i>')
         print(text_in_i)
+    return wrapper2
+
+def dec3(func3):
+    def wrapper3(s):
+        text_u = func3(s)
+        text_in_u = ('<u> ' + str(text_u) + ' </u>')
         print(text_in_u)
-    return wrapper
+    return wrapper3
 
-
-@decorator
+@dec1
+@dec2
+@dec3
 def func(s):
     s = 'Its a text from function'
     return s
+
 
 s = 0
 func(s)
