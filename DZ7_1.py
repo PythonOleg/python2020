b8057d8cb678666f10568e10e43baf46690f3e96
PythonOleg <66390707+PythonOleg@users.noreply.github.com>
2020-07-07 18:13:59 +0300

    Add files via upload

---
+++ DZ7_1.py
@@ -0,0 +1,64 @@
+class RomanNumber(int):
+    def __init__(self,other,type_n):
+        self.other = other
+
+        
+    @property
+    def roman(self):
+        return __self
+
+    @roman.setter
+    def roman(self,other,type_n):
+        self.other = other
+        num = int()
+
+    #def bin_number(self,other):
+#        return self.a + other
+
+        
+#    def __add__(self, other):
+#       return self.a + other
+
+#    def __sub__(self,other):
+#        return self.a - other
+
+#    def __div__(self,other):
+#        return self / other
+
+#    def __mul__(self, other):
+#        return self * other
+
+#    def __lt__(self,other):
+#        return self < other
+    
+#    def __iadd__(self,other):
+#        self += other
+#        return self
+
+#    def __eq__(self,other):
+#        return self == other
+
+#    def __ne__(self,other):
+#        return self != other
+
+#    def __gt__(self,other):
+#        return self > other
+
+
+n = input('Input number: ')
+
+if n:
+    print(str(n))
+    #roman_number = RomanNumber(n,type(n))
+else:
+    print('Is not digit!!!')
+
+
+
+
+
+
+    
+
+
+
