3cf90b432737a9249d233bca03af411b399cd6eb
PythonOleg <66390707+PythonOleg@users.noreply.github.com>
2020-07-08 15:37:35 +0300

    Add files via upload

---
+++ DZ8_1.py
@@ -0,0 +1,60 @@
+from os import open
+
+class MyOpen:
+    def __init__(self,f,right,encoding = 'utf-8'):
+        self.f = f
+        self.right = right
+      
+        os.open()
+
+    def open(self,right):
+        file.open(f,right)
+        return f
+
+    def read(self,number):
+        
+        for i in file:
+            f_file = file[i]
+            return f_file
+
+      
+    def readline(self):
+        
+        for line in f:
+
+            return 
+
+    def readlines(self):
+        return 
+
+    def write(self):
+        return
+
+    def writelines(self):
+        return 
+
+    def seek(self):
+        return
+
+    def tell(self):
+        return
+
+    def tell(self):
+
+        return 
+
+    def close(self):
+        return
+
+
+file = MyOpen('f://trialText.txt', 'rw')
+n = int(input('Input numbers of integers: '))
+file.open()
+file.read(n)
+file.readline()
+file.readlines()
+file.write()
+file.writelines()
+file.seek()
+file.close()
+
