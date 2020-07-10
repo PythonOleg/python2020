dec082ab6e81c11c047760894d60c228ce126aa3
PythonOleg <66390707+PythonOleg@users.noreply.github.com>
2020-07-10 23:49:50 +0300

    Add files via upload

--- DZ7_1_1.py
+++ DZ7_1_1.py
@@ -3,36 +3,32 @@ class Roman(int):
         self.num = number
         self.type = type
         #print(self.num,self.type)
-        
+
     def to_dec(self,roman2):
         self.num = int(self.num,self.type)
-        
-        
-    
+
+
+
     def __add__(self,roman2):
         res = self.num + roman2.num
-        
+
         return res
 
     def __sub__(self,roman2):
         res = self.num - roman2.num
-        
+
         return res
 
     def __mul__(self,roman2):
         res = self.num * roman2.num
         return res
 
-    def __div__(self,roman2):
-        res = self.num / roman2.num
+    def __truediv__(self,roman2):
+        res = self.num // roman2.num
         return res
 
     def __mod__(self,roman2):
         res = self.num % roman2.num
-        return res
-
-    def __pow__(self,roman2):
-        res = self.num ** roman2.num
         return res
 
     def __neg__(self):
@@ -44,26 +40,47 @@ class Roman(int):
             return 'First integer is less than second'
         else:
             return 'First integer is not less than second'
-    def __mt__(self,roman2):
+    def __gt__(self,roman2):
         if(self.num > roman2.num):
-            return 'First integer is more than second'
+            return True
         else:
-            return 'First integer is not more than second'
+            return False
+    def __eq__(self, roman2):
+        if (self.num == roman2.num):
+            return True
+        else:
+            return False
 
+    def __ne__(self, roman2):
+        if (self.num != roman2.num):
+            return True
+        else:
+            return False
+
+    def __le__(self, roman2):
+        if (roman.num <= roman2.num):
+            return True
+        else:
+            return False
+    def __ge__(self, roman2):
+        if (roman.num >= roman2.num):
+            return True
+        else:
+            return False
 
     def numToRoman(self,num):
         ones = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
         tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
         hunds = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
         thous = ["","M","MM","MMM","MMMM"]
-    
-        t = thous[n // 1000]
-        h = hunds[n // 100 % 10]
-        te = tens[n // 10 % 10]
-        o =  ones[n % 10]
-     
+
+        t = thous[self.num // 1000]
+        h = hunds[self.num // 100 % 10]
+        te = tens[self.num // 10 % 10]
+        o =  ones[self.num % 10]
+
         return t+h+te+o
-       
+
 number = input('Input number: ')
 number2 = input('Input second number: ')
 
@@ -85,21 +102,28 @@ roman2.to_dec(number2)
 res_add = roman.__add__(roman2)
 res_sub = roman.__sub__(roman2)
 res_mul = roman.__mul__(roman2)
-res_div = roman.__div__(roman2)
+res_div = roman.__truediv__(roman2)
 res_mod = roman.__mod__(roman2)
-res_pow = roman.__pow__(roman2)
 res_neg = roman.__neg__()
-res.
+res_lt = roman.__lt__(roman2)
+res_gt = roman.__gt__(roman2)
+res_eq = roman.__eq__(roman2)
+res_ne = roman.__ne__(roman2)
+res_le = roman.__le__(roman2)
+res_ge = roman.__ge__(roman2)
 
 
 
-print('Res Add: '+ str(roman + roman2))
-print('Res_Sub: '+ str(res_sub))
-print('Res mul: '+ str(res_mul))
-print('Res div: '+ str(res_div))
+
+print('Res Add: '+ str(roman + roman2)+'in roman: ' + str(roman.numToRoman(res_add)))
+print('Res_Sub: '+ str(roman - roman2)+'in roman: ' + str(roman.numToRoman(res_sub)))
+print('Res mul: '+ str(roman * roman2)+'in roman: ' + str(roman.numToRoman(res_mul)))
+print('Res div: '+ str(roman / roman2)+'in roman: ' + str(roman.numToRoman(res_div)))
 print('Res modul:' + str(res_mod))
-print('Res pow:' + str(res_pow))
-print('Res neg:' + str(res_neg))
-print(roman.num < roman2.num)
-print(roman.num > roman2.num)
-
+print('Res A neg:' + str(res_neg))
+print('less than number: '+ str(roman.num < roman2.num))
+print('more than number: '+ str(roman.num > roman2.num))
+print('equal: '+ str(roman.num == roman2.num))
+print('not equal: ' + str(roman.num != roman2.num))
+print('<= '+ str(roman.num <= roman2.num))
+print('>= '+ str(roman.num >= roman2.num))
