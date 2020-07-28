0f9b8dc5df46a3a0124ca3a0f09065440bf84374
PythonOleg <66390707+PythonOleg@users.noreply.github.com>
2020-07-29 00:29:21 +0300

    Add files via upload

--- DZ8_1.py
+++ DZ8_1.py
@@ -1,60 +1,58 @@
-from os import open
-
+import os
+#####################################Клас Myopen########################################################################
 class MyOpen:
-    def __init__(self,f,right,encoding = 'utf-8'):
-        self.f = f
-        self.right = right
-      
-        os.open()
-
-    def open(self,right):
-        file.open(f,right)
-        return f
-
-    def read(self,number):
-        
-        for i in file:
-            f_file = file[i]
-            return f_file
-
-      
-    def readline(self):
-        
-        for line in f:
-
-            return 
-
-    def readlines(self):
-        return 
+    def __init__(self):
+        self.address = address
 
     def write(self):
-        return
+        with open(address,'w') as file:
+            my_string = input('Input string: ')
+            file.write(my_string+'\n')
+            file.close()
 
     def writelines(self):
-        return 
+        n = int(input('Now many line you want to write? '))
+        with open(address,'w') as file:
+            for line in range(n):
+                string_line = input('Input line: ')
+                file.writelines(string_line)
+            file.close()
+
+    def read(address):
+        with open(address,'r') as file:
+            n = int(input('How many symbols must be readed? '))
+            content = file.read(n)
+            print(content)
+            file.close()
+
+    def readlines(self):
+        with open(address,'r') as file:
+            content = file.readline()
+            print(content)
+            file.close()
+
+    def readlines(self):
+        with open(address,'r') as file:
+            line = file.readline()
+            print(line)
 
     def seek(self):
-        return
+        with open(address) as file:
+            cursor = int(input('Input cursor position: '))
+            file.seek(cursor)
+            return cursor
 
     def tell(self):
-        return
+        print('Cursor position is ' + str(cursor))
+######################################Основна частина програми##########################################################
 
-    def tell(self):
-
-        return 
-
-    def close(self):
-        return
-
-
-file = MyOpen('f://trialText.txt', 'rw')
-n = int(input('Input numbers of integers: '))
-file.open()
-file.read(n)
-file.readline()
-file.readlines()
-file.write()
-file.writelines()
-file.seek()
-file.close()
-
+address = input('Input file address: ')
+n = 0
+cursor = 0
+file = MyOpen
+file.write(address)
+file.read(address)
+file.writelines(address)
+file.readlines(address)
+cursor = file.seek(address)
+file.tell(cursor)
